9a5cd3164da947595b975207b5c9a574ab08a260
zuhdil <zuhdil@gmail.com>
2023-01-25 18:21:07 +0700

    Implement index page

---
+++ src/components/pricing/pricing-tab/index.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import Button from "../../reusable/button";
+import { planData } from "../data";
+import Check from "../../../images/check.svg";
+import Cross from "../../../images/cross.svg";
+import "./index.scss";
+
+const PricingTab = () => {
+  const [selected, setSelected] = useState("flow-basic");
+  const selectedPlan = planData.find((plan) => plan.id === selected);
+
+  const isSelected = (plan) => selectedPlan.id === plan;
+
+  return (
+    <div className="pricing-mobile">
+      <ul className="tab-list">
+        <li
+          className={`list-item ${isSelected("flow-basic") && "selected"}`}
+          onClick={() => setSelected("flow-basic")}
+          onKeyDown={() => setSelected("flow-basic")}
+          role="presentation"
+        >
+          Flow Basic
+        </li>
+        <li
+          className={`list-item ${isSelected("flow-pro") && "selected"}`}
+          onClick={() => setSelected("flow-pro")}
+          onKeyDown={() => setSelected("flow-pro")}
+          role="presentation"
+        >
+          Flow Pro
+        </li>
+        <li
+          className={`list-item ${isSelected("flow-pro-plus") && "selected"}`}
+          onClick={() => setSelected("flow-pro-plus")}
+          onKeyDown={() => setSelected("flow-pro-plus")}
+          role="presentation"
+        >
+          Flow Custom
+        </li>
+      </ul>
+
+      <div className="plan">
+        {selected === "flow-pro" && (
+          <div className="most-popular">Most popular</div>
+        )}
+        <h3 className="plan-name">{selectedPlan.name}</h3>
+        <div className="plan-price">{selectedPlan.price}</div>
+        <small className="plan-note">{selectedPlan.note}</small>
+        <Button
+          type="filled"
+          text={
+            selectedPlan.name.toLowerCase() === "flow pro +"
+              ? "Contact sales"
+              : "Get started"
+          }
+          linkTo="/signup"
+        />
+      </div>
+
+      <table className="features-table">
+        <tbody>
+          <tr className="features-row">
+            <td className="feature">
+              <b>Features</b>
+            </td>
+            <td className="empty-row"></td>
+          </tr>
+          {selectedPlan.features.map((feature, index) => {
+            return (
+              <tr key={index} className="features-row">
+                <td className="feature-name">{feature.name}</td>
+                <td className="feature-value">
+                  {feature.value === "yes" ? (
+                    <div className="check">
+                      <Check />
+                    </div>
+                  ) : feature.value === "no" ? (
+                    <div className="cross">
+                      <Cross />
+                    </div>
+                  ) : (
+                    feature.value
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <small className="note">
+        *PowerBI integration will be gauged based on familiarity with the tool
+        by partner
+      </small>
+    </div>
+  );
+};
+
+export default PricingTab;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,23 @@
-import * as React from "react";
+import React from "react";
+import KeyFeatures from "../components/key-features";
 import Layout from "../components/layout";
+import Overview from "../components/overview";
+import PricingTab from "../components/pricing/pricing-tab";
+import PricingTable from "../components/pricing/pricing-table";
+import Subscribe from "../components/subscribe";
+import Testimonial from "../components/testimonial";
 
 const IndexPage = () => {
-  return <Layout />;
+  return (
+    <Layout>
+      <Overview />
+      <KeyFeatures />
+      <PricingTab />
+      <PricingTable />
+      <Testimonial />
+      <Subscribe />
+    </Layout>
+  );
 };
 
 export default IndexPage;
