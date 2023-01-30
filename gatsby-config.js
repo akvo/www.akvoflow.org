5f8a68e7fe9f4f3d0640cf7258742a5d4ef342a3
zuhdil <zuhdil@gmail.com>
2023-01-30 14:39:03 +0700

    Fix failed build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,10 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-
-const siteUrl = "https://www.akvoflow.org";
-const languages = ["en", "es", "fr"];
-const defaultLanguage = "en";
-
 module.exports = {
   siteMetadata: {
     title: "Akvo Flow Landing Page",
-    siteUrl,
+    siteUrl: "https://www.akvoflow.org",
   },
   plugins: [
     "gatsby-plugin-sass",
@@ -24,12 +19,12 @@ module.exports = {
     {
       resolve: "gatsby-plugin-react-i18next",
       options: {
-        languages,
-        defaultLanguage,
-        siteUrl,
+        languages: ["en", "es", "fr"],
+        defaultLanguage: "en",
+        siteUrl: "https://www.akvoflow.org",
         i18nextOptions: {
           //debug: true,
-          defaultNS: 'common',
+          defaultNS: "common",
           interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
           },
